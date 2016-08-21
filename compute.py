8999403a5c128f4817e836d1fb8ebfa7ae7ffa03
Joel B. Mohler <joel@kiwistrawberry.us>
2016-08-21 14:43:43 -0400

    fast ellipse computation and point dragging

--- compute.py
+++ compute.py
@@ -8,9 +8,11 @@ CANVAS_SIZE = 10
 foci = numpy.array([[3, 3], [3, -3], [-3, 3], [-3, -3]])
 C = 20.5
 
-def compute_boundary(foci, C):
+def _compute_boundary_random(foci, C, ctl, cbr):
     points = 100000
-    scatter = numpy.random.uniform(-CANVAS_SIZE/2, CANVAS_SIZE/2, points).reshape(points//2, 2)
+    x = numpy.random.uniform(ctl[0], cbr[0], points)
+    y = numpy.random.uniform(ctl[1], cbr[1], points)
+    scatter = numpy.array([p for p in zip(x, y)])
     #scatter = numpy.array([[1.2, 1]])
 
     #print(foci)
@@ -21,6 +23,33 @@ def compute_boundary(foci, C):
     #print(scatter[])
 
     return boundary
+
+def _foci_sum(pnt, foci):
+    return sum([dist2(foci[i]-pnt) for i in range(len(foci))])
+
+def _compute_boundary_centered(foci, C, ctl, cbr):
+    center = sum(numpy.array(foci))/len(foci)
+    boundary = []
+    iterations = int(math.log(C*2000))
+    for _theta in range(400):
+        theta = _theta/400.*2*math.pi
+        inner = center
+        outer = center + C*numpy.array([math.cos(theta), math.sin(theta)])
+        for i in range(iterations):
+            midpoint = (inner+outer)/2.
+            if _foci_sum(midpoint, foci) < C:
+                inner = midpoint
+            else:
+                outer = midpoint
+        boundary.append(inner)
+    return boundary
+
+def compute_boundary(foci, C, ctl, cbr):
+    center = sum(numpy.array(foci))/len(foci)
+    if _foci_sum(center, foci) < C:
+        return _compute_boundary_centered(foci, C, ctl, cbr)
+    else:
+        return _compute_boundary_random(foci, C, ctl, cbr)
 
 def write_png(boundary, foci, granularity=20):
     from PIL import Image, ImageDraw
--- main.py
+++ main.py
@@ -5,21 +5,61 @@ class EllipseWidget(QtGui.QWidget):
     def __init__(self, parent=None):
         super(EllipseWidget, self).__init__(parent)
 
+        self.circumference = 20
+
+        self.dragging = None
         self.foci = []
         self.boundary = None
 
     def mousePressEvent(self, event):
         if event.button() == QtCore.Qt.LeftButton:
-            self.foci.append(self.pos2p(event.pos()))
-            self.update()
+            coords = self.pos2p(event.pos())
+            for index, p in enumerate(self.foci):
+                if compute.dist2((p[0]-coords[0], p[1]-coords[1])) < 0.3:
+                    self.dragging = index
+                    self.update()
+                    return True
         return super(EllipseWidget, self).mousePressEvent(event)
+
+    def mouseMoveEvent(self, event):
+        if self.dragging != None:
+            new_foci = self.pos2p(event.pos())
+            self.foci[self.dragging] = new_foci
+            self.update()
+            self.compute_ellipse()
+        return super(EllipseWidget, self).mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            new_foci = self.pos2p(event.pos())
+            if self.dragging != None:
+                self.foci[self.dragging] = new_foci
+                self.dragging = None
+            else:
+                self.foci.append(new_foci)
+            self.update()
+            self.compute_ellipse()
+        if event.button() == QtCore.Qt.RightButton:
+            coords = self.pos2p(event.pos())
+            for index, p in enumerate(self.foci):
+                if compute.dist2((p[0]-coords[0], p[1]-coords[1])) < 0.3:
+                    del self.foci[index]
+                    self.update()
+                    self.compute_ellipse()
+                    return True
+        return super(EllipseWidget, self).mouseReleaseEvent(event)
 
     def mouseDoubleClickEvent(self, event):
         if event.button() == QtCore.Qt.LeftButton:
             print('computing')
             print(self.foci)
-            self.boundary = compute.compute_boundary(self.foci, 20)
-            self.update()
+            self.compute_ellipse()
+
+    def compute_ellipse(self):
+        cs1 = self.pos2p(self.rect().topLeft())
+        cs2 = self.pos2p(self.rect().bottomRight())
+        self.boundary = compute.compute_boundary(self.foci, self.circumference, cs1, cs2)
+        self.update()
 
     def pos2p(self, pos):
         x, y = pos.x(), pos.y()
@@ -34,16 +74,53 @@ class EllipseWidget(QtGui.QWidget):
     def paintEvent(self, event):
         qp = QtGui.QPainter()
         qp.begin(self)
+        radius = 3
         for f in self.foci:
-            qp.drawPoint(*self.p2pixel(f))
+            x, y = self.p2pixel(f)
+            qp.setBrush(QtGui.QBrush('blue'))
+            qp.drawEllipse(x-radius, y-radius, 2*radius, 2*radius)
         if self.boundary != None:
-            for f in self.boundary:
-                qp.drawPoint(*self.p2pixel(f))
+            qp.setPen(QtGui.QPen('blue'))
+            for index in range(len(self.boundary)):
+                p1 = self.p2pixel(self.boundary[index-1])
+                p2 = self.p2pixel(self.boundary[index])
+                qp.drawLine(QtCore.QPoint(*p1), QtCore.QPoint(*p2))
+            #for f in self.boundary:
+            #    qp.drawPoint(*self.p2pixel(f))
+        if len(self.foci) > 0:
+            xsum = sum([x for x, _ in self.foci])
+            ysum = sum([y for _, y in self.foci])
+            x = xsum / len(self.foci)
+            y = ysum / len(self.foci)
+            x, y = self.p2pixel((x, y))
+            qp.setPen(QtGui.QPen('maroon'))
+            qp.drawEllipse(x-radius, y-radius, 2*radius, 2*radius)
         qp.end()
+
+class EllipseStudio(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        super(EllipseStudio, self).__init__(parent)
+
+        self.wid = QtGui.QWidget()
+        self.layout = QtGui.QVBoxLayout(self.wid)
+        self.circ_edit = QtGui.QLineEdit()
+        self.ell_wid = EllipseWidget()
+        self.layout.addWidget(self.circ_edit)
+        self.layout.addWidget(self.ell_wid)
+
+        self.setCentralWidget(self.wid)
+
+        self.circ_edit.setText(str(self.ell_wid.circumference))
+
+        self.circ_edit.editingFinished.connect(self.recirc)
+
+    def recirc(self):
+        self.ell_wid.circumference = int(self.circ_edit.text())
+        self.ell_wid.compute_ellipse()
 
 if __name__ == '__main__':
     app = QtGui.QApplication([])
-    w = QtGui.QMainWindow()
-    w.setCentralWidget(EllipseWidget())
+    w = EllipseStudio()
+    w.resize(600, 500)
     w.show()
     app.exec_()
