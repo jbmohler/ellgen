9c8c15664308c67384ceef3fbcee8f63431960f9
Joel B. Mohler <joel@kiwistrawberry.us>
2024-01-06 23:38:45 -0500

    html frontend for ellipse foci placement

--- server.py
+++ server.py
@@ -3,30 +3,32 @@ import compute
 
 app = sanic.Sanic(__name__)
 
-app.static('/static', './static', name="get_static_files")
-app.static('/index.html', './index.html', name="get_index_html")
+app.static("/static", "./static", name="get_static_files")
+app.static("/index.html", "./index.html", name="get_index_html")
 
 
 @app.post("/api/compute", name="post_api_compute")
 def post_api_compute(request):
-    foci = [(f['x'], f['y']) for f in request.json]
+    foci = [(f["x"], f["y"]) for f in request.json]
 
     if len(foci) == 1:
         return sanic.response.json([])
 
     maxes = (
-            max(f[0] for f in foci),  max(f[1] for f in foci), 
-            )
+        max(f[0] for f in foci),
+        max(f[1] for f in foci),
+    )
 
-    C = len(foci)* 150
+    C = len(foci) * 150
 
     print(maxes, C)
 
-    b = compute.compute_boundary(foci, C, (0, 0), (maxes[0]*2, maxes[1]*2))
+    b = compute.compute_boundary(foci, C, (0, 0), (maxes[0] * 2, maxes[1] * 2))
 
-    b = [{'x': bp[0], 'y': bp[1]} for bp in b]
+    b = [{"x": bp[0], "y": bp[1]} for bp in b]
     print(b)
     return sanic.response.json(b)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, dev=True)
